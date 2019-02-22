d8e3d07ad5f09fef0d9b4e688bc7faf6afb46ad9
Zafar <zafar.a.ali2@gmail.com>
2019-02-22 17:47:14 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timerStarted: false,
+      timerStopped: true,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+      captures: []
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <h2 className="text-center">Timer with React</h2>
+        <div className="timer-container">
+          <div className="current-timer">
+            {this.state.hours + ":" + this.state.minutes + ":" + this.state.seconds}
+          </div>
+          <div className="timer-controls">
+            <button className="btn btn-success">
+              Start Timer
+            </button>
+            <button className="btn btn-alert">Stop Timer</button>
+            <button className="btn btn-info">Capture Time</button>
+            <button className="btn btn-danger">Reset Timer</button>
+
+
+          </div>
+        </div>
       </div>
     );
   }
